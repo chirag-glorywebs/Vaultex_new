bfc16c71b68cc943af47cfcb7815a1280b03fd43
Hardik Khorasiya <hardikkhorasiya09@gmail.com>
2021-11-19 00:03:25 +0530

    send otp fixed, send forget password email api create

--- app/Http/Controllers/API/VendorEmailController.php
+++ app/Http/Controllers/API/VendorEmailController.php
@@ -3,13 +3,16 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\API\BaseController;
+use App\Models\PasswordReset;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\DB;
 use App\Models\VendorEmail;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Str;
 
 class VendorEmailController extends BaseController
 {
@@ -36,21 +39,19 @@ class VendorEmailController extends BaseController
             $user->mobile = $request->mobile;
             $user->vendor_code = $request->code;
         }
-        $user->phone = $otp;
+        $user->otp = $otp;
         $user->save();
 
         $response = Http::get('https://smpplive.com/api/send_sms/single_sms', [
-            'to' => "91$request->mobile",
+            'to' => "91" . $request->mobile,
             'username' => 'inventmedia',
             'password' => 'In@R5304',
-            'from' => 'SMS',
+            'from' => 'SKYOTP',
             'content' => "$otp from Vaultex Project"
         ]);
 
-        Log::info($response->collect()->toArray());
-
         if ($response->successful()) {
-            return $this->sendResponse(true, "Success");
+            return $this->sendResponse(true, "OTP sent successfully");
         }
         return $this->sendError(true, $response->failed());
     }
@@ -65,19 +66,62 @@ class VendorEmailController extends BaseController
         $validator = Validator::make($request->all(), [
             'code' => 'required|string|min:3',
             'mobile' => 'required|string|min:3',
-            'otp' => 'required|string|min:3',
+            'otp' => 'required|string|digit_between:6,6',
         ]);
 
         $user = User::query()
             ->where('mobile', $request->mobile)
             ->where('vendor_code', $request->code)
-            ->where('phone', $request->otp)
+            ->where('otp', $request->otp)
             ->first();
 
         if ($user) {
-            return $this->sendResponse(true, "Success");
+            return $this->sendResponse(true, "OTP verify successfully");
         }
         return $this->sendError(false, 'Invalid OTP. Please try again');
+    }
+
+    /**
+     * Send Forget Password Email api
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function sendForgetPasswordEmail(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'code' => 'required|string|min:3',
+            'mobile' => 'required|string|min:3',
+            'otp' => 'required|string|min:3',
+            'email' => 'required|string|min:3',
+        ]);
+
+        $user = User::query()
+            ->where('mobile', $request->mobile)
+            ->where('vendor_code', $request->code)
+            ->where('otp', $request->otp)
+            ->first();
+
+        if (!$user) {
+            return $this->sendError(false, 'Record not found in database');
+        }
+        $user->email = $request->email;
+        $user->save();
+
+        $token = Str::random(60);
+        PasswordReset::create(
+            ['email' => $request->email, 'token' => $token, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()]
+        );
+
+        Mail::send('API.email.email-verify', [
+            'email' => $request->email,
+            'token' => $token,
+            'reset_url' => route('password.update', ['token' => $token, 'email' => $request->email]),
+        ], function ($message) use ($request) {
+            $message->subject('Reset Password Request');
+            $message->to($request->email);
+        });
+
+        return $this->sendResponse(true, "Mail send successfully");
     }
 
     /**
